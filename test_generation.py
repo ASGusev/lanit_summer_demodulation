1323a9bc9dbe7861152103884567c4aacea245cc
Andrey <andy.gusev@list.ru>
2020-08-17 22:06:55 +0300

    Refactor test generation script
    Target directory for the generated file extracted to a separate argument.

--- test_generation.py
+++ test_generation.py
@@ -1,5 +1,6 @@
 import json
 import argparse
+from pathlib import Path
 
 from demodulation.data_preprocessing import generate_samples_per_snr, convert_samples_to_json
 import modulation
@@ -13,7 +14,8 @@ if __name__ == '__main__':
                             help='Sound-noise ratios to generate samples for')
     arg_parser.add_argument('--shift', type=float, default=None,
                             help='Value that is added to all channels frequencies, as share of the channel width')
-    arg_parser.add_argument('--prefix', default='data/test', help='Prefix of the generated file name')
+    arg_parser.add_argument('--prefix', default='test', help='Prefix of the generated file name')
+    arg_parser.add_argument('--dir', default='data', help='Directory to place the generated file')
     args = arg_parser.parse_args()
 
     modulator = modulation.Modulator(frequency_shift=args.shift)
@@ -22,5 +24,7 @@ if __name__ == '__main__':
     shift_part = f'sh{int(args.shift * 100)}_' if args.shift else ""
     ratios_part = ",".join(map(str, args.snr))
     out_file_name = f'{args.prefix}_{ratios_part}_{shift_part}{args.n_samples}_{args.sample_len}.json'
-    with open(out_file_name, 'wt') as out_file:
+    out_dir_path = Path(args.dir)
+    out_file_path = out_dir_path / out_file_name
+    with out_file_path.open('wt') as out_file:
         json.dump(test_samples, out_file)
